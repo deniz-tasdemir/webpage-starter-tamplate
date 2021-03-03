353009cd2fd377455ab988529d075af94ad7acec
Deniz Taşdemir <deniztasdemir34@gmail.com>
2021-03-03 15:23:52 +0300

    genel düzenleme yapıldı

--- resources/views/layouts/site/bootstrap.blade.php
+++ resources/views/layouts/site/bootstrap.blade.php
@@ -1,18 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    @include('layouts.site.header')
-    <title>Hello, world!</title>
-  </head>
-  <body>
-  @include('layouts.site.navbar')  
-<div class="container">
-<h1>Bootstrap Dersleri</h1>
-</div>
- @include('layouts.site.footer') 
+@extends('layouts.site.master')
 
-  </body>
-</html>
+@section ('content')
+<div class="container">
+<h1>Bootstrap 4 Dersleri</h1>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsum laudantium asperiores ab aperiam libero eveniet nobis qui numquam ipsa. Corrupti repellat, recusandae nemo a nisi pariatur quas optio voluptas dolor rerum perspiciatis esse nihil minima odio fuga eos amet veniam. Aliquam, sequi odit, expedita impedit voluptas delectus recusandae sit minima voluptate excepturi rerum quasi quas optio deserunt iure repellendus explicabo nemo! Perspiciatis neque nihil asperiores facilis vel voluptate ea eaque error consequuntur mollitia quo reprehenderit obcaecati illo tenetur voluptatum eligendi debitis molestiae velit nam, suscipit blanditiis. Iste at mollitia recusandae est incidunt praesentium aspernatur illo explicabo debitis esse quos vitae, rerum sunt dignissimos provident labore dolorem nobis corporis eum, eius itaque possimus hic voluptatem inventore. Accusamus reprehenderit officia, facilis quae repudiandae recusandae deleniti aut libero impedit ab est dolor, eius minus ad, nemo a laborum veniam voluptatibus quasi cumque autem aperiam! Quod quam dolores, itaque rerum eaque sequi perspiciatis unde, deserunt tempora sunt rem optio debitis illo consequuntur autem distinctio laborum officiis nihil sint modi deleniti officia! Modi dolorem optio laboriosam exercitationem adipisci dignissimos minima, sed harum excepturi nisi. Doloremque quidem earum ducimus nulla minima fugiat aspernatur cupiditate laborum ullam perspiciatis asperiores unde quasi animi minus rem, eligendi, blanditiis amet a totam harum? A ea quibusdam quam eaque perferendis reprehenderit tenetur excepturi! Harum iure quae voluptatibus aut! Delectus omnis dolore harum quas tempore dolor laborum placeat et distinctio autem officiis facilis nam fuga eos deleniti, hic odit a, aliquam numquam voluptas? Corrupti ipsa autem fugit. Harum dolorem, suscipit tempore adipisci commodi ipsa aliquam quidem quis accusamus obcaecati, corporis consectetur repellat culpa enim molestiae animi asperiores. Dolor asperiores, quo mollitia odio voluptatibus ipsum voluptates commodi animi in, explicabo quidem ipsam unde alias consequuntur nobis nemo maiores. Doloremque veniam quas, excepturi ipsum odio consectetur enim placeat reprehenderit modi quia impedit sequi.
+</div>
+@endsection
