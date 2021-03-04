c33549d481a2de492cbc1d566e9dd99c7423abaf
Deniz Taşdemir <deniztasdemir34@gmail.com>
2021-03-04 15:56:30 +0300

    memberLogin and Register Form

--- resources/views/layouts/site/memberLogin.blade.php
+++ resources/views/layouts/site/memberLogin.blade.php
@@ -1,47 +1,59 @@
 @extends('layouts.site.master')
 
 @section ('content')
-<div class="memberLogin"></div>
-<div class="container">
-   <div class="c1">
-      
-      <div class="c11">
-         <h1 class="mainhead">PICK YOUR SPOT</h1>
-         <p class="mainp">Just click the buttons below to toggle between SignIn & SignUp</p>
-      </div>
-      <div id="left"><h1 class="s1class"><span>SIGN</span><span class="su">IN</span>
-      </h1></div>
-      <div id="right"><h1 class="s2class"><span>SIGN</span><span class="su">UP</span></h1></div>
-   </div>
-   <div class="c2">
-      <form class="signup">
-         <h1 class="signup1">SIGN UP</h1>
-         <br><br><br><br>
-			<input name="username" type="text" placeholder="Username*" class="username"/>
-			
-			<input name="email" type="text" placeholder="Email*" class="username"/>
-			
-			<input name="password" type="password" placeholder="Password*" class="username"/>
-         
-         <button class="btn">Sign Up</button>
-      </form>
-      <form class="signin">
-         
-         <h1 class="signup1">SIGN IN</h1>
-         <br><br><br><br>
-         <input name="username" type="text" placeholder="Username*" class="username"/>
-			
-			<input name="password" type="password" placeholder="Password*" class="username"/>
-         
-         <button class="btn">Get Started</button>
-         
-         <br><br><br><br>
-         <a href=""><p class="signup2">Forget Password?</p></a>
-      </form>
-      
-   </div>
-</div>
-</div
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt">
+					<img src="images/site/images/memberlogin.png" alt="memberLogin">
+				</div>
+
+				<form class="login100-form validate-form">
+					<span class="login100-form-title">
+						Üye Girişi
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Email">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Giriş Yap
+						</button>
+					</div>
+
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Unuttum
+						</span>
+						<a class="txt2" href="#">
+							Kullanıcı / Şifre?
+						</a>
+					</div>
+
+					<div class="text-center p-t-136">               
+						<a class="txt2" href="{{ url('/signUp') }}" >
+							Kayıt Ol
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 @endsection
 
 
